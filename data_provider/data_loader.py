e2f2732551b56519934792babe4debf0cb0c247f
Tamas Jambor <jamborta@gmail.com>
2024-09-27 22:29:42 +0100

    get all data

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -415,8 +415,8 @@ class Dataset_PA(Dataset):
             self.label_len = size[1]
             self.pred_len = size[2]
         # init
-        assert flag in ["train", "test", "val"]
-        type_map = {"train": 0, "val": 1, "test": 2}
+        assert flag in ["train", "test", "val", "all"]
+        type_map = {"train": 0, "val": 1, "test": 2, "all": 3}
         self.set_type = type_map[flag]
 
         self.features = features
@@ -456,8 +456,8 @@ class Dataset_PA(Dataset):
         num_train = int(len(df_raw) * 0.7)
         num_test = int(len(df_raw) * 0.2)
         num_vali = len(df_raw) - num_train - num_test
-        border1s = [0, num_train - self.seq_len, len(df_raw) - num_test - self.seq_len]
-        border2s = [num_train, num_train + num_vali, len(df_raw)]
+        border1s = [0, num_train - self.seq_len, len(df_raw) - num_test - self.seq_len, 0]
+        border2s = [num_train, num_train + num_vali, len(df_raw), len(df_raw)]
         border1 = border1s[self.set_type]
         border2 = border2s[self.set_type]
 
